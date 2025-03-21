5bf61305f17f32e553dcd9265a01f1be4d23dc88
Ruu <3884018146@qq.com>
2025-03-22 03:31:27 +0800

    feat:添加进程检测

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,8 @@
 // You should have received a copy of the GNU General Public License along
 // with EfficientScheduler. If not, see <https://www.gnu.org/licenses/>.
 
+use std::{fs, process::exit};
+
 use anyhow::Result;
 
 mod framework;
@@ -30,8 +32,31 @@ fn wait_boot() {
     }
 }
 
+fn check_process() {
+    let mut count = 0;
+    if let Ok(entries) = fs::read_dir("/proc") {
+        for entry in entries.flatten() {
+            let pid = entry.file_name().into_string().unwrap_or_default();
+            if pid.parse::<u32>().is_err() {
+                continue;
+            }
+            if let Ok(cmdline) = fs::read_to_string(format!("/proc/{pid}/cmdline")) {
+                if cmdline.contains("EfficientScheduler") {
+                    count += 1;
+                }
+            }
+        }
+    }
+    if count > 1 {
+        eprintln!("发现另一个进程，程序退出");
+        exit(1);
+    }
+}
+
 fn main() -> Result<()> {
     logger::log_init()?;
+    wait_boot();
+    check_process();
     framework::scheduler::Scheduler::try_start_scheduler()?;
     Ok(())
 }
