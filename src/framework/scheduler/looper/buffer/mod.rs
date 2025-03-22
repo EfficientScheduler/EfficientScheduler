6f6c2e31e3423d49c02ddebcf2757452b11cc1c9
Ruu <3884018146@qq.com>
2025-03-22 14:03:23 +0800

    feat:添加对uclamp的调控

---
+++ src/framework/scheduler/looper/buffer/mod.rs
@@ -0,0 +1,143 @@
+// Copyright 2023-2025, [rust@localhost] $ (@3532340532)
+//
+// This file is part of EfficientScheduler.
+//
+// EfficientScheduler is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// EfficientScheduler is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along
+// with EfficientScheduler. If not, see <https://www.gnu.org/licenses/>.
+
+#![allow(dead_code)]
+#![allow(clippy::nursery, clippy::pedantic)]
+
+use std::{ffi::CString, fs, os::unix::fs::PermissionsExt, ptr};
+
+use anyhow::Result;
+use libc::{MS_BIND, MS_REC, mount, umount, umount2};
+use serde::Deserialize;
+
+const TMGP: &str = include_str!("./tmgp.toml");
+
+#[derive(Deserialize)]
+struct Cpuctl {
+    bg_uclamp: Uclamp,
+    ta_uclamp: Uclamp,
+    fg_uclamp: Uclamp,
+}
+
+#[derive(Deserialize)]
+struct Uclamp {
+    max: usize,
+    min: usize,
+}
+
+pub struct Buffer {
+    cpuctl: Cpuctl,
+}
+
+impl Buffer {
+    pub fn new() -> Self {
+        Self {
+            cpuctl: Cpuctl {
+                bg_uclamp: Uclamp { max: 0, min: 0 },
+                ta_uclamp: Uclamp { max: 0, min: 0 },
+                fg_uclamp: Uclamp { max: 0, min: 0 },
+            },
+        }
+    }
+
+    fn set_uclamp(&self, cpuctl: Cpuctl) {
+        let operations = [
+            (
+                "/dev/cpuctl/background/cpu.uclamp.max",
+                &cpuctl.bg_uclamp.max,
+            ),
+            (
+                "/dev/cpuctl/background/cpu.uclamp.min",
+                &cpuctl.bg_uclamp.min,
+            ),
+            (
+                "/dev/cpuctl/foreground/cpu.uclamp.max",
+                &cpuctl.fg_uclamp.max,
+            ),
+            (
+                "/dev/cpuctl/foreground/cpu.uclamp.min",
+                &cpuctl.fg_uclamp.min,
+            ),
+            ("/dev/cpuctl/top-app/cpu.uclamp.max", &cpuctl.ta_uclamp.max),
+            ("/dev/cpuctl/top-app/cpu.uclamp.min", &cpuctl.ta_uclamp.min),
+        ];
+        for (path, value) in operations {
+            if let Err(e) = fs::set_permissions(path, fs::Permissions::from_mode(0o644)) {
+                log::error!("无法设置权限 {}: {}", path, e);
+            }
+            if let Err(e) = fs::write(path, value.to_string()) {
+                log::error!("无法写入文件 {}: {}", path, e);
+            }
+        }
+    }
+
+    pub fn set_uclamp_topapps(&self, topapps: &str) {
+        if topapps == "com.tencent.tmgp.pubgmhd" {
+            let cpuctl: Cpuctl = toml::from_str(TMGP).unwrap();
+            self.set_uclamp(cpuctl);
+        }
+    }
+}
+
+pub fn lock_value(path: &str, value: &str) -> Result<()> {
+    let mount_path = format!("/cache/mount_mask_{value}");
+    unmount(path)?;
+    if let Err(e) = fs::set_permissions(path, fs::Permissions::from_mode(0o644)) {
+        log::error!("无法设置权限{}: {e}", path);
+    }
+    if let Err(e) = fs::write(path, value) {
+        log::error!("无法写入文件{}: {e}", path);
+    }
+    if let Err(e) = fs::set_permissions(path, fs::Permissions::from_mode(0o444)) {
+        log::error!("无法设置权限{}: {e}", path);
+    }
+    if let Err(e) = fs::write(&mount_path, value) {
+        log::error!("无法写入文件{}: {e}", mount_path);
+    }
+    mount_bind(&mount_path, path)?;
+    Ok(())
+}
+
+fn mount_bind(src_path: &str, dest_path: &str) -> Result<()> {
+    let src_path = CString::new(src_path)?;
+    let dest_path = CString::new(dest_path)?;
+
+    unsafe {
+        umount2(dest_path.as_ptr(), libc::MNT_DETACH);
+
+        if mount(
+            src_path.as_ptr().cast(),
+            dest_path.as_ptr().cast(),
+            ptr::null(),
+            MS_BIND | MS_REC,
+            ptr::null(),
+        ) != 0
+        {
+            return Err(std::io::Error::last_os_error().into());
+        }
+    }
+
+    Ok(())
+}
+
+fn unmount(file_system: &str) -> Result<()> {
+    let path = CString::new(file_system)?;
+    if unsafe { umount(path.as_ptr()) } != 0 {
+        return Err(std::io::Error::last_os_error().into());
+    }
+    Ok(())
+}
